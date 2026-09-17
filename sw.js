94be616180fa8cb23db19c2e46de5d994ecedaaf
painelwn777 <painelwn777@gmail.com>
2026-09-17 16:00:44 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,39 +1,52 @@
-const CACHE_NAME = 'pix-demo-v1';
-
-const FILES = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png'
+const CACHE_NAME = 'santander-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './santander.png',
+  './logo.png',
+  './comprovante.png',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
+// Instalar o Service Worker e cachear os arquivos
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES);
-        })
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto com sucesso');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => console.log('Erro ao cachear arquivos:', err))
+  );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
-            )
-        )
-    );
-    self.clients.claim();
-});
-
+// Interceptar requisições e servir do cache
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Limpar caches antigos
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
